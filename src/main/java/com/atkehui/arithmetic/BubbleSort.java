842ed8121ad1e35dfd516fdcfbbde8d98c17e4b2
kehui <574059694@qq.com>
2019-11-21 14:10:58 +0800

    选择排序

--- src/main/java/com/atkehui/arithmetic/BubbleSort.java
+++ src/main/java/com/atkehui/arithmetic/BubbleSort.java
@@ -6,6 +6,11 @@ import java.util.Arrays;
  * @author eternity
  * @create 2019-11-21 10:29
  */
+
+/**
+ * 冒泡排序（Bubble Sorting）的基本思想是：通过对待排序序列从前向后（从下标较小的元素开始）,依次比较
+ * 相邻元素的值，若发现逆序则交换，使值较大的元素逐渐从前移向后部，就象水底下的气泡一样逐渐向上冒。
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {-2, 6, 4, 10, 3, -1, 2, -3, -2};
@@ -19,6 +24,12 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     * 如果一趟比较下
+     * 来没有进行过交换，就说明序列有序，因此要在排序过程中设置
+     * 一个标志flag判断元素是否进行过交换。从而减少不必要的比较。
+     * @param arr
+     */
     public static void bubbleSort(int[] arr) {
         int temp = 0;//临时变量
         boolean flag = false;//定义一个boolean标识变量，用来判断过程中是否还有交换，用来优化代码
---
+++ src/main/java/com/atkehui/arithmetic/SelectionSort.java
@@ -0,0 +1,42 @@
+package com.atkehui.arithmetic;
+
+/**
+ * @author eternity
+ * @create 2019-11-21 11:30
+ */
+
+import java.util.Arrays;
+
+/**
+ * 选择式排序 也属于内部排序法，是从欲排序的数据中，按指定的规则选出某一元素，再依规定交换位置后达到排序的目的。
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {-2, 6, 4, 10, 3, -1, 2, -3, -2};
+        System.out.println("**********排序前**********");
+        System.out.println(Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println("**********排序后**********");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];//假定最小值
+            int minIndex = i;//最小值的索引
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {//说明假定的最小值并不是最小
+                    min = arr[j];//重置min
+                    minIndex = j;//更新最小值的索引
+                }
+            }
+            if (minIndex != i) {//将最小值放在arr[i],即交换
+                arr[minIndex] = arr[i];
+                arr[i] = min;
+            }
+        }
+    }
+}
