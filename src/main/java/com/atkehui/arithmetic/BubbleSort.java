57675bcf1d75df30f132cedd1821d38b7e9487eb
kehui <574059694@qq.com>
2019-11-21 11:16:47 +0800

    冒泡排序

--- src/main/java/com/atkehui/arithmetic/BubbleSort.java
+++ src/main/java/com/atkehui/arithmetic/BubbleSort.java
@@ -15,27 +15,27 @@ public class BubbleSort {
 
         bubbleSort(arr);
 
+        System.out.println("**********排序后**********");
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void bubbleSort(int[] arr) {
-        int temp = 0;
-        boolean falg = false;//定义一个boolean变量，用来判断过程中是否还有交换，用来优化代码
+        int temp = 0;//临时变量
+        boolean flag = false;//定义一个boolean标识变量，用来判断过程中是否还有交换，用来优化代码
         for (int i = 1; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    falg = true;
+                    flag = true;
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-            if (!falg) {
+            if (!flag) {//在一趟排序中,一次都没有交换过
                 break;
-            } else {
-                falg = false;
+            } else {//如果交换过，将标识变量重置
+                flag = false;
             }
         }
-        System.out.println("**********排序后**********");
-        System.out.println(Arrays.toString(arr));
     }
 }
